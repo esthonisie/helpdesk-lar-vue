20c375b0912e8665032085ee83f361e934d97f4c
esthonisie <132005743+esthonisie@users.noreply.github.com>
2026-03-10 15:17:08 +0100

    Initial commit

---
+++ resources/js/bootstrap.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosStatic } from 'axios';
+
+declare global {
+	interface Window {
+		axios: AxiosStatic;
+	}
+}
+
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
